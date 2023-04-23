432d00cd4f3a5a5a1b672ae1f67398f2dfb8b0b3
lin <18064505@qq.com>
2023-04-23 08:55:54 +0200

    feat: test 2 print logid

--- middleware/ctx_http_handler/ctx_handler.go
+++ middleware/ctx_http_handler/ctx_handler.go
@@ -3,6 +3,7 @@ package ctx_http_handler
 import (
 	"context"
 	uuid "github.com/satori/go.uuid"
+	"log"
 	"net/http"
 )
 
@@ -11,6 +12,7 @@ func HandleFunc(pattern string, handler func(ctx context.Context, w http.Respons
 		pattern,
 		func(w http.ResponseWriter, r *http.Request) {
 			ctx := context.WithValue(context.Background(), "logID", uuid.NewV1())
+			log.Printf("-------> logID: %v", ctx.Value("logID"))
 			handler(ctx, w, r)
 		},
 	)
