8112e29ceef16f9521482fa12428af499bb03e66
lin <18064505@qq.com>
2023-05-04 08:54:13 +0800

    feat: sysctl in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+sysctl net.ipv4.tcp_keepalive_time
 sysctl -w net.ipv4.tcp_keepalive_time=200
 sysctl -w net.ipv4.tcp_keepalive_intvl=200
 sysctl -w net.ipv4.tcp_keepalive_probes=5
-/app/main -privileged
+/app/main
