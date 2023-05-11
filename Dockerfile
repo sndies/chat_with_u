6cb3439142931e4fcbc369617f85e12804cbb4dc
lin <18064505@qq.com>
2023-05-12 00:02:17 +0800

    feat: add release.yml v12

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ COPY . /app/
 
 # 执行代码编译命令。操作系统参数为linux，编译后的二进制产物命名为main，并存放在当前目录下。
 RUN GOOS=linux
+RUN CGO_ENABLED=0
 #go build -o main .
 
 # 选用运行时所用基础镜像（GO语言选择原则：尽量体积小、包含基础linux内容的基础镜像）
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ sysctl net.ipv4.tcp_keepalive_time
 sysctl -w net.ipv4.tcp_keepalive_time=200
 sysctl -w net.ipv4.tcp_keepalive_intvl=200
 sysctl -w net.ipv4.tcp_keepalive_probes=5
-/app/main
+/bin/bash ./main
