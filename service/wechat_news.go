dd18c17d6c4171727c658661bb99417a653f5543
lin <18064505@qq.com>
2023-06-25 17:13:46 +0800

    feat: 调整代码格式

--- service/wechat_news.go
+++ service/wechat_news.go
@@ -29,9 +29,8 @@ type NewsReq struct {
 
 func HandleWechatNews(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 	log.Infof(ctx, "receive wechat news, raw_r: %+v", r)
-	//glog.Infof("receive wechat news, raw_r: %+v", r)
 
-	// 这段是用来接入微信开发者验证的
+	// === 特殊: 这段是用来接入微信开发者验证的 ===
 	if r.Method == http.MethodGet {
 		echoStr := r.URL.Query().Get("echostr")
 		w.WriteHeader(200)
@@ -39,19 +38,18 @@ func HandleWechatNews(ctx context.Context, w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	// 从xml解析为结构体
+	// 1.从xml解析为结构体
 	reqJson, err := model.NewMsg(ctx, r)
 	if err != nil {
 		echo(w, []byte("success"))
 		return
 	}
 	log.Infof(ctx, "receive json req: %s", utils.ToJsonString(reqJson))
-	//glog.Infof("receive json req: %s", utils.ToJsonString(reqJson))
 
-	// 调用处理逻辑
+	// 2.调用处理逻辑
 	reply := queryAndWrapRes(ctx, reqJson)
 
-	// 返回结果
+	// 3.返回结果
 	if reply != "" {
 		echo(w, reqJson.GenerateEchoData(ctx, reply))
 	}
@@ -61,38 +59,37 @@ func queryAndWrapRes(ctx context.Context, req *model.Msg) (reply string) {
 	// 出口日志
 	start := time.Now()
 	defer log.Infof(ctx, "[queryAndWrapRes] req: %s, reply: %s, cost: %v", utils.ToJsonString(req), reply, time.Since(start))
-	//defer glog.Infof("[queryAndWrapRes] req: %s, reply: %s, cost: %v", utils.ToJsonString(req), reply, time.Since(start))
 
-	// 检查入参
+	// 1.检查入参
 	req.Content = strings.TrimSpace(req.Content)
 	if pass, errMsg := checkReqMsg(ctx, req.Content); !pass {
 		return errMsg
 	}
 
-	// 同一用户对话的串行控制,先用内存吧,后续迁移到mysql
+	// 2.同一用户对话的串行控制,暂时先用内存,后续可以迁移到mysql
 	_, ok := cache.Get(ctx, req.FromUserName)
 	if ok {
 		reply = "上个问题正在处理中，请稍等..."
 		return
 	}
 
-	// 将user放入缓存
+	// 3.将user放入缓存
 	_ = cache.Add(ctx, req.FromUserName, true, time.Second*10)
 
-	// 用msgId查询数据库
+	// 4.用msgId查询数据库
 	qna, err := dao.GetGptQNAByMsgId(ctx, req.MsgId)
 	if qna != nil {
-		// 如果已经有答案直接返回
+		// 4.1 如果已经有答案直接返回
 		if qna.Answer != "" {
 			reply = qna.Answer
 			return
 		}
-		// 没有答案,告诉用户正在处理中
+		// 4.2 没有答案,告诉用户正在处理中
 		reply = "收到，请稍等..."
 		return
 
 	} else {
-		// 如果没有答案, 写记录，先返回处理中
+		// 4.3 如果没有答案, 写记录，先返回处理中
 		err = dao.InsertGptQNA(ctx, &db_model.GptQNA{
 			MsgId:        req.MsgId,
 			FromUserName: req.FromUserName,
@@ -106,27 +103,46 @@ func queryAndWrapRes(ctx context.Context, req *model.Msg) (reply string) {
 		reply = "收到，请稍等..."
 	}
 
-	// 异步
+	// 5.异步请求gpt
 	utils.SafeGo(ctx, func() {
 		log.Infof(ctx, "[queryAndWrapRes] 进入异步流程")
-		// 发起请求
+		defer cache.Del(ctx, req.FromUserName) // 删除缓存,用户可以发起下一次请求
+		// 5.1 发起请求
 		reply, err = gpt_handler.Completions(ctx, req.Content, nil)
 		if err != nil || len(reply) == 0 {
 			return
 		}
-		// 写进数据库
+		// 5.2 写进数据库
 		err = dao.UpdateAnswerByMsgId(ctx, reply, req.MsgId)
 		if err != nil {
 			return
 		}
 		log.Infof(ctx, "[queryAndWrapRes] 写数据库成功")
-		// 将结果主动推送给用户
+		// 5.3 将结果主动推送给用户
 		_ = SendMsgToUser(ctx, req.FromUserName, reply)
-		// 删除缓存,用户可以发起下一次请求
-		cache.Del(ctx, req.FromUserName)
+
 	})
 
 	return
+}
+
+func checkReqMsg(ctx context.Context, msg string) (bool, string) {
+	length := len([]rune(msg))
+
+	if length <= 1 {
+		return false, "请说详细些..."
+	}
+	if length > consts.MaxQuestionLength {
+		return false, "问题字数超出设定限制，请精简问题"
+	}
+
+	return true, ""
+}
+
+func echo(w http.ResponseWriter, data []byte) {
+	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(data)
 }
 
 func invokeCompletion(ctx context.Context, req *model.Msg) string {
@@ -164,23 +180,4 @@ func invokeCompletion(ctx context.Context, req *model.Msg) string {
 	}
 
 	return ""
-}
-
-func checkReqMsg(ctx context.Context, msg string) (bool, string) {
-	length := len([]rune(msg))
-
-	if length <= 1 {
-		return false, "请说详细些..."
-	}
-	if length > consts.MaxQuestionLength {
-		return false, "问题字数超出设定限制，请精简问题"
-	}
-
-	return true, ""
-}
-
-func echo(w http.ResponseWriter, data []byte) {
-	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(data)
 }
